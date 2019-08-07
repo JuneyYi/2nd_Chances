8c314b6abcc89bd803715b926fb5723075503fde
juney <juhnnie@gmail.com>
2019-08-06 18:24:10 -0700

    dsa

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Animal.findAll({}).then(function(dbAnimal) {
+      res.json(dbAnimal);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Animal.create(req.body).then(function(dbAnimal) {
+      res.json(dbAnimal);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Animal.destroy({ where: { id: req.params.id } }).then(function(dbAnimal) {
+      res.json(dbAnimal);
     });
   });
 };
